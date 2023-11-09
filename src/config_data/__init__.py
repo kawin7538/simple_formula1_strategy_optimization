96414f25cd6cfc426e17267fb7056ddec9a9c136
kawin7538 <kawin7538@hotmail.com>
2023-11-09 15:54:37 +0700

    update basic structure of car and config data

---
+++ src/config_data/__init__.py
@@ -0,0 +1,47 @@
+DICT_ENGINE_MODE={
+    'name':[
+        
+    ],
+    'base_reliability_percent_loss_per_lap':[
+
+    ],
+    'base_maximum_horsepower':[
+
+    ],
+    'fuel_volume_consuming_kg_per_lap':[
+
+    ],
+    'temperature_celcius':[
+
+    ],
+}
+
+DICT_TYRE_SET={
+    'name':[
+
+    ],
+    'base_reliability_percent_loss_per_lap':[
+
+    ],
+    'optimal_temperature_celcius':[
+
+    ],
+    'laptime_loss_per_lap_seconds':[
+
+    ]
+}
+
+DICT_BRAKE_MODE={
+    'name':[
+
+    ],
+    'base_brake_pressure_psi':[
+
+    ],
+    'base_brake_temperature_celcius':[
+
+    ],
+    'base_reliability_percent_loss_per_lap':[
+        
+    ]
+}
--- src/models/car.py
+++ src/models/car.py
@@ -1,5 +1,15 @@
-from engine import Engine
+from models.engine import Engine
+from models.tyres import Tyres
+from models.brakes import Brakes
+
+from config_data import DICT_ENGINE_MODE, DICT_TYRE_SET, DICT_BRAKE_MODE
 
 class Car:
     def __init__(self) -> None:
         self.base_weight_kg=700
+
+        self.engine=Engine()
+        self.tyres=Tyres()
+        self.brakes=Brakes()
+
+        self.engine.init_engine_mode(**DICT_ENGINE_MODE)
