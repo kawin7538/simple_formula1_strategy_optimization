e38acb8e9a370dbe1cf25f9977916040fd12c07f
kawin7538 <kawin7538@hotmail.com>
2023-11-15 13:59:49 +0700

    update f1_env

---
+++ src/main_f1_env_player.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+from f1_env.f1_env import F1Env
+
+env=F1Env()
+
+observation, info = env.reset()
+
+answer_reward=0
+for i in range(66*28):
+    action=env.action_space.sample()
+    observation, reward, terminated, _, _ = env.step(action)
+
+    if not terminated:
+        answer_reward+=reward
+    else:
+        print("Car DNF at lap",i//28)
+        print("Time Elapsed before DNF",timedelta(seconds=-answer_reward))
+        answer_reward=reward
+        break;
+print("sum answer_reward",answer_reward)
